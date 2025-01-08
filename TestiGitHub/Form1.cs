8f7214266d8c87b86d4049dbf462956fa62dd53e
maramanaa <127495912+maramanaa@users.noreply.github.com>
2025-01-08 12:52:05 +0200

    second branch

--- TestiGitHub/Form1.cs
+++ TestiGitHub/Form1.cs
@@ -11,7 +11,7 @@ namespace TestiGitHub
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+//second branch komentara
         }
 
         private void button1_Click(object sender, EventArgs e)
